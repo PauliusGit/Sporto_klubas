75b72bab2692bea011746982baa8d31018bd2182
paulius <paulius.burnys@gmail.com>
2021-03-01 14:12:56 +0200

    adding footer header and navbar

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="<?php echo URLROOT; ?>"><?php echo SITENAME; ?></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="<?php echo URLROOT; ?>">Titulinis</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo URLROOT; ?>/pages/about">Apie Mus</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link " aria-current="page" href="<?php echo URLROOT; ?>/users/register">Prisijungti</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="<?php echo URLROOT; ?>/users/login">Registruotis</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
