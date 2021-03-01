5e950c928166974a6ce21b0824e331b13dbf3011
paulius <paulius.burnys@gmail.com>
2021-03-01 13:29:42 +0200

    created config file with constant and added index functionality

---
+++ app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Sporto_klubas');
+
+// aprrot wil be used when we need absloute path to our app dir
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// URL ROOT will be the path in the url
+define('URLROOT', 'http://localhost/Sporto_klubas');
+
+// Site name 
+define('SITENAME', 'Sporto klubas Aurora');
+
+
