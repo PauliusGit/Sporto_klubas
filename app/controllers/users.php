0f5e3dad09cf1aa8e9d6262aad32cdd7a12e4d73
paulius <paulius.burnys@gmail.com>
2021-03-01 17:26:32 +0200

    created users controller and added user model

---
+++ app/controllers/users.php
@@ -0,0 +1,144 @@
+<?php 
+
+class Users extends Controller
+{
+    public function __construct()
+    {
+        $this->userModel = $this->model('User');
+    }
+
+    public function register()
+    {
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'firstName' => trim($_POST['firstName']),
+                'lastName' => trim($_POST['lastName']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'phoneNr' => trim($_POST['phoneNr']),
+                'address' => trim($_POST['address']),
+                'firstName_err' => '',
+                'lastName_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            if(empty($data['email'])) {
+                $data['email_err'] = 'Įveskite elektroninį paštą';
+            } elseif(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+                $data['email_err'] = 'Netinkamas El.Pašto formatas';
+            }
+            else {
+                if($this->userModel->findUserByEmail($data['email'])){
+                    $data['email_err'] = 'El.Pašto adresas jau naudojamas';
+                }
+            }
+
+            if(empty($data['firstName'])) {
+                $data['firstName_err'] = 'Įveskite vardą';
+            }else{
+                if(strlen($data['firstName']) > 40){
+                    $data['firstName_err'] = 'vardas negali viršyti 40 simbolių';
+                }
+            }
+
+            if(empty($data['lastName'])) {
+                $data['lastName_err'] = 'Įveskite pavardę';
+            }else{
+                if(strlen($data['lastName']) > 40){
+                    $data['lastName_err'] = 'vardas negali viršyti 40 simbolių';
+                }
+            }
+
+            if(empty($data['password'])) {
+                $data['password_err'] = 'Įveskite slaptažodį';
+            }elseif(strlen($data['password']) < 6) {
+                $data['password_err'] = 'Password must be at least 6 characters';
+            }
+
+
+            if(empty($data['email_err']) && empty($data['firstName_err']) && empty($data['lastName_err']) && empty($data['password_err'])) {
+                $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+                if($this->userModel->register($data)){
+                    header('Location:' . URLROOT . '/users/login');
+                }else {
+                    die('something went wrong');
+                }
+            } else {
+                $this->view('users/register', $data);
+            } 
+
+        }else {
+            $data = [
+                'firstName' => '',
+                'lastName' => '',
+                'email' => '',
+                'password' => '',
+                'phoneNr' => '',
+                'address' => '',
+                'firstName_err' => '',
+                'lastName_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            $this->view('users/register', $data);
+        }
+    }
+
+    public function login()
+    {
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            if(empty($data['email'])) {
+                $data['email_err'] = 'Įveskite elektroninį paštą';
+            }elseif(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+                $data['email_err'] = 'Netinkamas El.Pašto formatas';
+            }
+
+            if(empty($data['password'])) {
+                $data['password_err'] = 'Įveskite slaptažodį';
+            }
+
+            if($this->userModel->findUserByEmail($data['email'])){
+
+            }else{
+                $data['email_err'] = 'Toks El.Pašto adresas nerastas';
+            }
+
+            if(empty($data['email_err']) && empty($data['password_err'])) {
+                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                if($loggedInUser){
+                    die('success');
+                } else {
+                    $data['password_err'] = 'Slaptažodis neteisingas';
+                    $this->view('users/login', $data);
+                }
+            } else {
+                $this->view('users/login', $data);
+            } 
+
+        }else {
+            $data = [
+                'email' => '',
+                'password' => '',
+                'email_err' => '',
+                'password_err' => '',
+            ];
+
+            $this->view('users/login', $data);
+        }
+    }
+}
--- app/models/User.php
+++ app/models/User.php
@@ -30,15 +30,32 @@ class User
 
     public function register($data)
     {
-        $this->db->query('INSERT INTO users (firstname, lastname, email, password) VALUES (:firstname, :lastname, :email, :password)');
+        $this->db->query('INSERT INTO users (firstname, lastname, email, password, phoneNr, address) VALUES (:firstname, :lastname, :email, :password, :phoneNr, :address)');
         $this->db->bind(':firstname', $data['firstName']);
         $this->db->bind(':lastname', $data['lastName']);
         $this->db->bind(':email', $data['email']);
         $this->db->bind(':password', $data['password']);
+        $this->db->bind(':phoneNr', $data['phoneNr']);
+        $this->db->bind(':address', $data['address']);
 
         if($this->db->execute()) {
             return true;
         } else {
+            return false;
+        }
+    }
+
+    public function login($email, $password)
+    {
+        $this->db->query('SELECT * FROM users WHERE email = :email');
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->singleRow();
+
+        $hashed_password = $row->password;
+        if(password_verify($password, $hashed_password)) {
+            return $row;
+        }else{
             return false;
         }
     }
